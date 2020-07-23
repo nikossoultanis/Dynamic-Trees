05edaa17fabe03b99c76d4eccd5076150a654d88
Nikos Soultanis <fosdrogg@gmail.com>
2020-07-23 12:24:32 +0300

    final
    final

--- lct.cpp
+++ lct.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cstdio>
 #include <bits/stdc++.h> 
 #include <stdlib.h>
 #include <vector>
 #include <cassert>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
+#include <boost/graph/graphviz.hpp>
 
 using namespace boost;
 
@@ -38,31 +40,31 @@ static std::map<int, vertex_path> paths;
 static std::map<int, vertex_path>::iterator map_it_new;
 static std::map<int, vertex_path>::iterator map_it_old;
 
-Vertex makepath(Graph &g) // works
+Vertex makepath(Graph &g) 
 {
     id_counter++;
     path_counter++;
     Vertex u1 = add_vertex(g);
     g[u1].data = id_counter;
     g[u1].parent = NULL;
-    g[u1].cost = 0;
+    g[u1].cost = rand()% 100 + 1;
     g[u1].in_path = path_counter;
     vertex_path new_path;
     new_path.path.push_back(u1);
-    new_path.successor = NULL;
+    new_path.successor = u1;
     paths[path_counter] = new_path;
 
     return u1;
 }
 
-Vertex findtail(std::vector<Vertex> path) // works
+Vertex findtail(std::vector<Vertex> path)
 {
     if(path.size() > 0)
         return path.back();
     return NULL;
 }
 
-Vertex findhead(std::vector<Vertex> path) // works
+Vertex findhead(std::vector<Vertex> path)
 {
     if(path.size() > 0)
         return path.front();
@@ -70,7 +72,7 @@ Vertex findhead(std::vector<Vertex> path) // works
 }
 
 
-int findpathcost(std::vector<Vertex> path, Graph g) // works
+int findpathcost(std::vector<Vertex> path, Graph g)
 {
     int cost = 0;
     for(std::vector<Vertex>::iterator it = path.begin(); it != path.end(); ++it)
@@ -78,22 +80,34 @@ int findpathcost(std::vector<Vertex> path, Graph g) // works
     return cost;
 }
 
-void addpathcost(std::vector<Vertex> path, Graph &g, int cost) // works
+void addpathcost(std::vector<Vertex> path, Graph &g, int cost)
 {
     for(std::vector<Vertex>::iterator it = path.begin(); it != path.end(); ++it)
         g[*it].cost += cost;
 }
 
-std::vector<Vertex> join(std::vector<Vertex> p1, Vertex u, std::vector<Vertex> p2, Graph &g)
+std::vector<Vertex> join(std::vector<Vertex> p1, std::vector<Vertex> v, std::vector<Vertex> p2, Graph &g)
 {
+    Vertex u;
+    if(v.size() > 0)
+    {
+        u = v.front();
+    }
+    if(p1.size() < 1 && p2.size() < 1)
+    {
+        return paths[g[u].in_path].path;
+    }
     if(p1.size() > 0)
     {
         adjacency_list<vecS, vecS, bidirectionalS, Node, int>::vertex_descriptor v2 = u;
         Vertex p1_tail = findtail(p1);
         Vertex p2_head = findhead(p2);
         Vertex p1_succ = g[p1_tail].parent;
-        boost::add_edge(p1_tail, u, g);
-        boost::add_edge(u, p2_head, g);
+        if(g[p1_tail].data != g[u].data && !boost::edge(p1_tail,u,g).second && !boost::edge(u,p1_tail,g).second)
+            boost::add_edge(p1_tail, u, g[p1_tail].cost,g);
+        if(g[u].data != g[p2_head].data && !boost::edge(u,p2_head,g).second && !boost::edge(p2_head,u,g).second)   
+            boost::add_edge(u, p2_head, g[u].cost, g);
+
         for(std::vector<Vertex>::iterator it = p2.begin(); it != p2.end(); ++it)
         {
             g[*it].in_path = g[p1_tail].in_path;
@@ -120,10 +134,10 @@ std::vector<Vertex> join(std::vector<Vertex> p1, Vertex u, std::vector<Vertex> p
             p2_head = u;
         else
             p2_head = findhead(p2); // index for the map
-        std::cout << "head is : " << g[p2_head].data << std::endl;
         if(g[p2_head].in_path != g[u].in_path)
-        {
-            boost::add_edge(u, p2_head, g);
+        {  
+            if(!boost::edge(u,p2_head,g).second && !boost::edge(p2_head,u,g).second)
+                boost::add_edge(u, p2_head, g);
             vertex_path new_path;
             std::vector<Vertex> full_path;
             full_path.push_back(u);
@@ -144,12 +158,12 @@ std::vector<Vertex> join(std::vector<Vertex> p1, Vertex u, std::vector<Vertex> p
 
 std::pair<std::vector<Vertex>, std::vector<Vertex> > split(Vertex u, Graph &g)
 {
+    // remove_edge(u, v, g);
     std::vector<Vertex> origin, p, q;
     origin = paths[g[u].in_path].path;
     std::vector<Vertex>::iterator index;
     if(g[u].data < 1 )
         return ( std::pair<std::vector<Vertex>,std::vector<Vertex> >(p,q) );
-
     for(std::vector<Vertex>::iterator it = origin.begin(); it != origin.end(); ++it)
     {
         if(*it == u)
@@ -160,11 +174,15 @@ std::pair<std::vector<Vertex>, std::vector<Vertex> > split(Vertex u, Graph &g)
     }
     for(std::vector<Vertex>::iterator it1 = origin.begin(); it1 != index; ++it1)
         p.push_back(*it1);
+
     for(std::vector<Vertex>::iterator it2 = index+1; it2 != origin.end(); ++it2) 
          q.push_back(*it2);
 
-    map_it_old = paths.find(g[u].in_path);
-    paths.erase(map_it_old);
+    if(paths[g[u].in_path].path.size() > 1) // if it's not a solo Vertex
+    {
+        map_it_old = paths.find(g[u].in_path);
+        paths.erase(map_it_old);
+    }
 
     if(!p.empty())
     {
@@ -181,7 +199,6 @@ std::pair<std::vector<Vertex>, std::vector<Vertex> > split(Vertex u, Graph &g)
     {
         vertex_path new_path;
         path_counter++;
-        std::cout << "counter : " << path_counter << std::endl;
         for(std::vector<Vertex>::iterator it1 = q.begin(); it1 != q.end(); ++it1)
             g[*it1].in_path = path_counter;
         new_path.path = q;
@@ -191,33 +208,23 @@ std::pair<std::vector<Vertex>, std::vector<Vertex> > split(Vertex u, Graph &g)
     return ( std::pair<std::vector<Vertex>,std::vector<Vertex> >(p,q) );
 }
 
-
-
-
 std::vector<Vertex> expose(Vertex u, Graph &g)
 {
     std::pair<std::vector<Vertex>,std::vector<Vertex> > cutted_paths; // cutted paths are p,q
     std::vector<Vertex> p,q,r;
     Vertex w;
     while(g[u].data != 0)
-    {   
+    {  
         w = paths[g[u].in_path].successor;
         cutted_paths = split(u, g);
         q = cutted_paths.first;
         r = cutted_paths.second;
-        std::cout << "r: " << r.size() << std::endl;
-        std::cout << "q: " << q.size() << std::endl;
-
         if(!q.empty())
         {
             paths[g[findtail(q)].in_path].successor = u;
         }
-        std::cout << "prin to join p: " << p.size() << std::endl;
-        std::cout << "to u einai to : " << g[u].data<< std::endl;
-        p = join(p, u, r, g);
-        std::cout << "meta to join p: " << p.size() << std::endl;
-
-        if(g[u].parent == NULL)
+        p = join(p, paths[g[u].in_path].path, r, g);
+        if(g[u].parent == 0)
         {
             paths[g[findtail(p)].in_path].successor = NULL;
             return p;
@@ -225,128 +232,76 @@ std::vector<Vertex> expose(Vertex u, Graph &g)
     }
 return p;
 }
+// void link(Vertex u, Vertex w)
+// {
 
-void cut(Vertex u, Graph &g) // needs expose
+// }
+
+void cut(Vertex u, Graph &g)
 {
     std::pair<std::vector<Vertex>,std::vector<Vertex> > cutted_paths; // cutted paths are p,q
-    std::vector<Vertex> arg = expose(u, g);
     cutted_paths = split(u, g);
+    if(!cutted_paths.first.empty())
+    {
+        remove_edge((Vertex)cutted_paths.first.front(), u, g);
+    }
+    if(!cutted_paths.second.empty())
+    {   
+        remove_edge(u, (Vertex)cutted_paths.second.back(), g);
+    }
     paths[g[u].in_path].successor =  NULL;
     Vertex temp = findtail(cutted_paths.second);
     paths[g[temp].in_path].successor = NULL;
 }
 
+bool visualize(Graph &g, bool term)
+{
+    if(!term){
+        if( remove( "graph.dot" ) != 0 )
+            perror( "Error deleting file" );
+        else
+            puts( "File successfully deleted" );
+        freopen("graph.dot","w",stdout);
+        write_graphviz(std::cout, g, default_writer(), 
+            make_label_writer(boost::get(edge_bundle,g))); 
+    }
+    else
+    {
+        write_graphviz(std::cout, g, default_writer(), 
+            make_label_writer(boost::get(edge_bundle,g)));
+    }
+    
+    return true;
+}
 // TODO when i join two paths, check if a vertex already exists
 
 int main(){
     Graph g;
 
     std::pair<vertex_iterator, vertex_iterator> vp;
+
+    Vertex x0 = makepath(g);
+    g[x0].is_root = 1;
+    g[x0].parent = NULL;
     Vertex x1 = makepath(g);
-    // g[x1].is_root = 1; // my root
-    // g[x1].parent = NULL;
-
-    makepath(g);
-    Vertex x = makepath(g);
-    makepath(g);
-    makepath(g);
-
-    // for(vp=vertices(g); vp.first!=vp.second; ++vp.first)
-    // {
-	// 	std::cout << g[*vp.first].in_path << " ";
-    //     std::cout << (paths[g[*vp.first].in_path].successor) << std::endl;
-    // }
-    std::vector<Vertex> p1 = paths[1].path;
-    std::vector<Vertex> p2 = paths[2].path;
-    std::vector<Vertex> final_p;
-    std::vector<Vertex> p3;
-    final_p = join(p1, x, p2, g);
-
-    // std::cout << g[0].data << std::endl;
-    // std::cout << final_p.size() << std::endl;
-    // std::cout << "final path is: ";
-    // for(std::vector<Vertex>::iterator it = paths[g[x].in_path].path.begin(); it != paths[g[x].in_path].path.end(); ++it)
-    // {
-    //     if(paths[g[*it].in_path].successor != NULL)
-    //         std::cout << paths[g[x].in_path].successor <<" ";
-    // }
-
-    // std::cout << g[findtail(paths[g[x1].in_path].path)].data << std::endl;
-    // std::cout << g[x1].data << std::endl;
-    // std::vector<Vertex> p = expose(x1, g);
-
-    //expose(x1, g);
-    // cut(x1,g);
+    Vertex x2 = makepath(g);
+    Vertex x3 = makepath(g);
+    Vertex x4 = makepath(g);
+    Vertex x5 = makepath(g);
     Vertex x6 = makepath(g);
-    for(std::vector<Vertex>::iterator it = paths[g[x1].in_path].path.begin(); it != paths[g[x1].in_path].path.end(); ++it)
+
+    Vertex allNodes[100000];
+    allNodes[0] = x0;
+    for (int i=1; i<100000; i++)
+        allNodes[i] = makepath(g);
+    
+    for (int i=0; i<50000; i++)
     {
-        std::cout << g[*it].data << std::endl;
+        join(paths[g[allNodes[i]].in_path].path,paths[g[allNodes[i+1]].in_path].path,paths[g[allNodes[i+2]].in_path].path, g);
     }
-    expose(x6, g);
-
-    // // std::cout << g[x1].in_path << std::endl;
-    // std::cout << "x6 anhkei" << g[x6].in_path << std::endl;
-    // std::cout << "x1 anhkei" << g[x1].in_path << std::endl;
-    for(std::vector<Vertex>::iterator it = paths[g[x1].in_path].path.begin(); it != paths[g[x1].in_path].path.end(); ++it)
+    for (int i=0; i<25000; i++)
     {
-        std::cout << g[*it].data << std::endl;
+        cut(allNodes[i], g);
     }
-
-    // std::cout << paths[g[x].in_path].path.size();
-    // std::cout << paths[findtail(paths[g[x].in_path].path)].successor << std::endl;
-    // std::cout << g[x].data << std::endl;
-    // std::cout << "" << std::endl;
-
-    // std::vector<Vertex> p;
-    // std::cout << "x path is: ";
-    // for(std::vector<Vertex>::iterator it = paths[g[x].in_path].path.begin(); it != paths[g[x].in_path].path.end(); ++it)
-    // {
-    //     std::cout << g[*it].data << " ";
-    // }
-    // std::cout << "" << std::endl;
-    // std::cout << "cutting path finished" << std::endl;
-    // std::pair<std::vector<Vertex>,std::vector<Vertex> > cutted_paths;
-
-    // cutted_paths = split(x, g);
-    // std::cout << "splitted path: ";
-    // for(std::vector<Vertex>::iterator it = (cutted_paths.first).begin(); it != (cutted_paths.first).end(); ++it)
-    // {
-    //     std::cout << g[*it].data << " ";
-    // }
-    // std::cout << "" << std::endl;
-
-
-
-    // for(std::vector<Vertex>::iterator it = final_p.begin(); it != final_p.end(); ++it)
-    // {
-    //    std::cout << paths[g[*vp.first].in_path].successor << std::endl;
-    // }
-
-    // for(std::vector<Vertex>::iterator it = final_p.begin(); it != final_p.end(); ++it)
-    // {
-    //     std::cout << g[*it].cost << std::endl;
-    // }
-
-    // adjacency_list<vecS, vecS, bidirectionalS, Node, int>::vertex_descriptor v1 = boost::add_vertex(g);
-    // adjacency_list<vecS, vecS, bidirectionalS, Node, int>::vertex_descriptor v2 = boost::add_vertex(g);
-
-    // std::pair<boost::adjacency_list<vecS, vecS, bidirectionalS, Node, int>::edge_descriptor, bool> p = boost::add_edge(v1, v2, g);
-    // std::cout << boost::edge(v2,v1,g).second <<std::endl;
-
-    // addpathcost(paths[g[1].in_path], g, 666);
-    // int cost = findpathcost(paths[g[1].in_path], g);
-
-    // Vertex u = findtail(paths[i]);
-    // std::cout << g[u].data << std::endl;
-    // for(std::vector<Vertex>::iterator it = paths[g[1].in_path].begin(); it != paths[g[1].in_path].end(); ++it)
-    // {
-    //     std::cout << g[*it].cost << std::endl;    
-    // }
-    // for (int i = 1; i <= paths.size(); i++)
-    // {
-
-    // }
-
-
     return 1;
 }
